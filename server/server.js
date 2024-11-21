f3fffadfc4c9875f9047767a582c98c122a2881c
pranavmanga <123859934+pranavmanga@users.noreply.github.com>
2024-11-21 15:01:32 +0530

    backend completed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const userRoutes = require("./routes/userRoutes"); // Import the router file
+const userRoutes = require("./routes/userRoutes");
+const parkingRoutes = require("./routes/parkingRoutes"); // Import parking routes
 const errorHandler = require('./middlewares/errorHandler');
 const connectDb = require("./config/dbConnection"); 
 
@@ -10,16 +11,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001; 
 connectDb(); 
+
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // API Routes
-app.use('/api/users', userRoutes); // Mount the user routes
+app.use('/api/users', userRoutes); // User routes
+app.use('/api/parking', parkingRoutes); // Parking routes
 
-// Test route
+// Test routes
 app.get('/', (req, res) => {
-    res.send("working");
+    res.send("API is working");
 });
 
 // Error handling middleware
@@ -29,4 +32,3 @@ app.use(errorHandler);
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
 });
-
